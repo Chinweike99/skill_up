026b286fc423ba289fbbfa07ca554b6aacda1a10
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-19 12:49:45 +0100

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './Components/NavBar/NavBar';
+import Footer from './Components/Footer/Footer';
+import Home from './Pages/Home';
+import Courses from './Pages/Courses';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/courses' element={<Courses />}/>
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
 }
