70674fad71456a4d1e5d65cec9498e71dc597ea0
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-21 10:42:25 +0100

    hero

--- client/src/Components/Home/Hero/Hero.jsx
+++ client/src/Components/Home/Hero/Hero.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import './Hero.css'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className='heroContainer'>
+        <div className='overlay'></div>
+
+        <div className='heroWrite'>
+          <h1>Unlock your potentials</h1>
+          <h3>Learn. Grow. Thrive</h3>
+          <p>Empowering you to become the best version of yourself..</p>
+
+          <p>Explore a wide range of skills across various fields like tech, creative arts, business, and more. With personalized learning paths and mentorship, you can achieve your goals faster.</p>
+        </div>
+
+        <button>Get started</button>
+      </div>
+    </div>
   )
 }
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../Components/Home/About/About.jsx'
 import Featured from '../Components/Home/Featured/Featured.jsx'
+import { Hero } from '../Components/Home/Hero/Hero.jsx'
 
 const Home = () => {
   return (
     <div>
+        <Hero />
         <About />
         <Featured />
     </div>
