fce0960784ca83ada80afceec141af096c99cd7e
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-21 06:45:50 +0100

    dropdown

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './NavBar.css'
+import { assets } from '../../assets/assets.js'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const NavBar = () => {
+  const [showlist, setShowList] = useState(false)
   return (
-    <div>NavBar</div>
+    <nav>
+      <div className='container'>
+        <img src={assets.skill} alt="" />
+
+        <div className='navList2' style={{display: showlist? "none" : "block"}}>
+          <CloseIcon
+          onClick={()=>setShowList(!showlist)}
+          className='closeIcon' 
+          style={{color: "#fff", fontSize: "3rem"}}/>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Courses</a></li>
+            <li><a href="">Explore</a></li>
+            <li><a href="">Communities</a></li>
+            <li><a href="">Resources</a></li>
+          </ul>
+        </div>
+
+
+        <MenuIcon
+        onClick={()=>setShowList(!showlist)}
+        className='menuIcon'
+        style={{color: "#fff", fontSize: "3rem"}}/>
+
+        <div className='navList'>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Courses</a></li>
+            <li><a href="">Explore</a></li>
+            <li><a href="">Communities</a></li>
+            <li><a href="">Resources</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -3,12 +3,13 @@ import dance from './dance.png'
 import fashionDesign from './fashionDesign.png'
 import fitness from './fitness.png'
 import fitness_dumbell from './fitness_dumbell.png'
-import Hairdresser from './marketing .png'
+import Hairdresser from './Hairdresser.png'
+import marketing from './marketing.png'
 import music from './music.png'
 import music_piano from './music_piano.png'
 import skill from './skill.png'
 import skill1 from './skill1.png'
-import skillup from './skillup.png'
+import skillup2 from './skillup2.png'
 import spa from './spa.png'
 import tailoring from './tailoring.png'
 import tech from './tech.png'
@@ -17,5 +18,5 @@ import webdevelopment from './webdevelopment.png'
 import writing from './writing.png'
 
 export const assets = {
-    barbing, dance, fashionDesign, fitness, fitness_dumbell,Hairdresser, music, music_piano, skill, skill1, skillup, spa, tailoring, tech, trade, webdevelopment, writing
+    barbing, dance, fashionDesign, fitness, fitness_dumbell,Hairdresser, music, music_piano, skill, skill1, skillup2, spa, tailoring, tech, trade, webdevelopment, writing, marketing
 }
