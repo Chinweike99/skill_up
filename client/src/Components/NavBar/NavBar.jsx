6de0997c9217248965b3b316ef9fe50b7f8ae6e7
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-21 08:55:51 +0100

    Navbar complete

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './NavBar.css'
 import { assets } from '../../assets/assets.js'
 import MenuIcon from '@mui/icons-material/Menu';
@@ -6,22 +6,38 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const NavBar = () => {
   const [showlist, setShowList] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 720) {
+        window.location.reload(); // Reload page to reset state when screen gets wider
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    
+    // Clean up event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <nav>
       <div className='container'>
         <img src={assets.skill} alt="" />
 
-        <div className='navList2' style={{display: showlist? "none" : "block"}}>
+        <div className='navList2' style={{display: showlist? "block" : "none"}}>
           <CloseIcon
           onClick={()=>setShowList(!showlist)}
           className='closeIcon' 
           style={{color: "#fff", fontSize: "3rem"}}/>
           <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Courses</a></li>
-            <li><a href="">Explore</a></li>
-            <li><a href="">Communities</a></li>
-            <li><a href="">Resources</a></li>
+            <li className='popIn'><a href="">Home</a></li>
+            <li className='popIn'><a href="">Courses</a></li>
+            <li className='popIn'><a href="">Explore</a></li>
+            <li className='popIn'><a href="">Communities</a></li>
+            <li className='popIn'><a href="">Resources</a></li>
           </ul>
         </div>
 
