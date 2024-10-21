5b56242990a07c7ca6d27f71ed13fef9e88df4c7
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-21 05:30:33 +0100

    assets

---
+++ client/src/assets/assets.js
@@ -0,0 +1,21 @@
+import barbing from './barbing.png'
+import dance from './dance.png'
+import fashionDesign from './fashionDesign.png'
+import fitness from './fitness.png'
+import fitness_dumbell from './fitness_dumbell.png'
+import Hairdresser from './marketing .png'
+import music from './music.png'
+import music_piano from './music_piano.png'
+import skill from './skill.png'
+import skill1 from './skill1.png'
+import skillup from './skillup.png'
+import spa from './spa.png'
+import tailoring from './tailoring.png'
+import tech from './tech.png'
+import trade from './trade.png'
+import webdevelopment from './webdevelopment.png'
+import writing from './writing.png'
+
+export const assets = {
+    barbing, dance, fashionDesign, fitness, fitness_dumbell,Hairdresser, music, music_piano, skill, skill1, skillup, spa, tailoring, tech, trade, webdevelopment, writing
+}
