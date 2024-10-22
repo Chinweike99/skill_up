bcf289a93b0610980efc5b0f5428980d64c5e880
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-22 11:45:19 +0100

    Benefit components

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import About from '../Components/Home/About/About.jsx'
 import Featured from '../Components/Home/Featured/Featured.jsx'
 import { Hero } from '../Components/Home/Hero/Hero.jsx'
+import Benefits from '../Components/Home/Benefits/Benefits.jsx'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero />
         <About />
         <Featured />
+        <Benefits />
     </div>
   )
 }
