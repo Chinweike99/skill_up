d8f40d56c532cb320b02c546591596f119755df7
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-22 02:03:23 +0100

    features update

--- client/src/assets/assetsVideos.js
+++ client/src/assets/assetsVideos.js
@@ -2,8 +2,10 @@ import aboutus from './aboutus.mp4'
 import marketing from './marketing.mp4'
 import programming from './programming.mp4'
 import writing from './writing.mp4'
+import entertainment from './entertainment.mp4'
+import fashion from './fashion.mp4'
 
 
 export const assetsVideo = {
-    aboutus, marketing, programming, writing
+    aboutus, marketing, programming, writing, fashion, entertainment
 }
