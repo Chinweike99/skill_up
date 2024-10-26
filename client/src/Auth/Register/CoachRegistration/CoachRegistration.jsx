cbfcc0a84bbff117226f5f99f52380c989b8e8c0
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-26 16:36:36 +0100

    Coach reg

--- client/src/Auth/Register/CoachRegistration/CoachRegistration.jsx
+++ client/src/Auth/Register/CoachRegistration/CoachRegistration.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './CoachRegistration.css'
 
 const CoachRegistration = () => {
   return (
-    <div>CoachRegistration</div>
+    <div className='studentReg'>
+      <div className='studentRegCon'>
+        <form>
+        <h4>Welcome to our Tutors programme</h4>
+        <div className='coachReg'>
+         <input type="text" placeholder='username'/>
+         <input type="text" placeholder='Field'/>
+        </div>
+          <input type="email" placeholder='@example.com'/>
+          <input type="password" placeholder='Password'/>
+          <input type="password" placeholder='Confirm Password'/>
+          <button> Submit</button>
+          <p>Have an account? <span>sign in</span></p>
+        </form>
+        
+      </div>
+    </div>
   )
 }
 
