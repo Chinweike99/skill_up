148648e5a5585f0b6d97c272c24fc59250fbfc35
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-28 21:37:25 +0100

    Updated

--- client/src/Pages/Courses.jsx
+++ client/src/Pages/Courses.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Courses = () => {
   return (
     <div>
-       <h1>COURSES STILL IN PRODUCTION</h1>
+       <h1>COURSES STILL IN DEVELOPRMENT</h1>
     </div>
   )
 }
