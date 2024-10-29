e373f87d0ab10b3ba35d76bbfcc0fd3966df0271
Chinweike99 <innocentakwolu1@gmail.com>
2024-10-29 20:08:49 +0100

    Side bar

--- client/src/Dashboards/CoachDashBoard/CoachSideBar/CoachSiderBar.jsx
+++ client/src/Dashboards/CoachDashBoard/CoachSideBar/CoachSiderBar.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import SchoolIcon from '@mui/icons-material/School';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import HistoryIcon from '@mui/icons-material/History';
+import './CoachSide.css'
 
 const CoachSiderBar = () => {
   return (
-    <div>CoachSiderBar</div>
+    <div className='sideBar'>
+        <div className='CoachSide'>
+            <LocalFireDepartmentIcon className='Coachicon'/>
+            <span>General</span>
+        </div>
+        
+        <div className='CoachSide'>
+            <DashboardIcon className='Coachicon'/>
+            <span>Dashboard</span>
+        </div>
+        <div className='CoachSide'>
+            <LocalLibraryIcon className='Coachicon'/>
+            <span>My courses</span>
+        </div>
+        <div className='CoachSide'>
+            <SchoolIcon className='Coachicon'/>
+            <span>Rooms</span>
+        </div>
+        <div className='CoachSide'>
+            <HistoryIcon className='Coachicon'/>
+            <span>History</span>
+        </div>
+        <div className='CoachSide' style={{marginBottom:"6rem"}}>
+            <SettingsIcon className='Coachicon'/>
+            <span> Settings</span>
+        </div>
+        <div className='CoachSide' style={{marginBottom:"rem"}}>
+            <HelpOutlineIcon className='Coachicon'/>
+            <span>Help</span>
+        </div>
+
+    </div>
   )
 }
 
